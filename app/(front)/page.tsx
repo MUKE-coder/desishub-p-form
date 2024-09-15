08d244763ba8fdf1ad2052161c43038e6211d574
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-09-16 01:11:19 +0300

    test

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,34 +1,45 @@
-import { getMembers } from "@/Actions/memberActions";
-import BoldHeading from "@/components/BoldHeading";
-import PerformanceTrackingForm from "@/components/PerformanceForm";
-import { ArrowLeft, ArrowRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+// import { getMembers } from "@/Actions/memberActions";
+// import BoldHeading from "@/components/BoldHeading";
+// import PerformanceTrackingForm from "@/components/PerformanceForm";
+// import { ArrowLeft, ArrowRight } from "lucide-react";
+// import Image from "next/image";
+// import Link from "next/link";
 
-export default async function Home() {
+// export default async function Home() {
+//   return (
+//     <main className="">
+//       <div className="flex justify-end  px-4">
+//         <Link
+//           className="inline-flex items-center px-4 py-2 
+//         bg-blue-600 text-white rounded-full
+//         shadow-lg hover:bg-blue-700 transition-colors duration-200
+//         font-semibold text-sm
+//         focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50
+//       "
+//           href="/reports"
+//         >
+//           <span>Table of Reports</span> <ArrowRight className="h-4 w-4" />
+//         </Link>
+//       </div>
+//       <div className="text-center">
+//         <BoldHeading heading="Desishub Daily Performance Report" />
+//       </div>
+//       <div></div>
+//       <div className="py-8">
+//         <PerformanceTrackingForm />
+//         <h2>hello world</h2>
+//       </div>
+//     </main>
+//   );
+// }
+
+import React from 'react'
+
+export default function page() {
   return (
-    <main className="">
-      <div className="flex justify-end  px-4">
-        <Link
-          className="inline-flex items-center px-4 py-2 
-        bg-blue-600 text-white rounded-full
-        shadow-lg hover:bg-blue-700 transition-colors duration-200
-        font-semibold text-sm
-        focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50
-      "
-          href="/reports"
-        >
-          <span>Table of Reports</span> <ArrowRight className="h-4 w-4" />
-        </Link>
-      </div>
-      <div className="text-center">
-        <BoldHeading heading="Desishub Daily Performance Report" />
-      </div>
-      <div></div>
-      <div className="py-8">
-        <PerformanceTrackingForm />
-        <h2>hello world</h2>
-      </div>
-    </main>
-  );
+    <div>
+      <p>hey</p>
+    </div>
+  )
 }
+
